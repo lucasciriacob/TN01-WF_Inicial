8f8f87c8112ac4a54c0ac314dada91bf5ca770f2
lucasciriacob <ciriacoborba077@gmail.com>
2025-02-28 22:08:04 -0300

    28/02 - Iniciado o Ex7 e criado um menu para todos os sistemas

---
+++ WFInicial/FormEx7.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WFInicial
+{
+    partial class FormEx7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMenor = new Button();
+            lblProduto = new Label();
+            lblPreco = new Label();
+            txtNome = new TextBox();
+            txtPreco = new TextBox();
+            SuspendLayout();
+            // 
+            // btnMenor
+            // 
+            btnMenor.Location = new Point(35, 139);
+            btnMenor.Name = "btnMenor";
+            btnMenor.Size = new Size(198, 23);
+            btnMenor.TabIndex = 0;
+            btnMenor.Text = "Mostrar o mais barato";
+            btnMenor.UseVisualStyleBackColor = true;
+            // 
+            // lblProduto
+            // 
+            lblProduto.AutoSize = true;
+            lblProduto.Location = new Point(24, 56);
+            lblProduto.Name = "lblProduto";
+            lblProduto.Size = new Size(103, 15);
+            lblProduto.TabIndex = 1;
+            lblProduto.Text = "Nome do Produto";
+            // 
+            // lblPreco
+            // 
+            lblPreco.AutoSize = true;
+            lblPreco.Location = new Point(27, 98);
+            lblPreco.Name = "lblPreco";
+            lblPreco.Size = new Size(100, 15);
+            lblPreco.TabIndex = 2;
+            lblPreco.Text = "Preço do Produto";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(133, 53);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 3;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(133, 95);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(100, 23);
+            txtPreco.TabIndex = 4;
+            // 
+            // FormEx7
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNome);
+            Controls.Add(lblPreco);
+            Controls.Add(lblProduto);
+            Controls.Add(btnMenor);
+            Name = "FormEx7";
+            Text = "FormEx7";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnMenor;
+        private Label lblProduto;
+        private Label lblPreco;
+        private TextBox txtNome;
+        private TextBox txtPreco;
+    }
+}
